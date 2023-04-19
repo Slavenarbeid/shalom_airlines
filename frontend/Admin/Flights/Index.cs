d5ffad5addce7a2863ceb903c80cb7ff8865d66d
TijmenSosef <tijmenpower@gmail.com>
2023-04-19 14:26:15 +0200

    flight search ding broke

--- frontend/Admin/Flights/Index.cs
+++ frontend/Admin/Flights/Index.cs
@@ -8,19 +8,50 @@ public class Index : Window
 {
     public Index()
     {
+        Layout.OpenWindow<Admin.Flights.Index>(null);
+    }
+    
+    public Index(Dictionary<string, object>? filters)
+    {
+
         Title = "Flights";
-        
-        var list = new ListView(Flight.All())
+
+        var SearchFieldText = new TextField("")
         {
+            Width = Dim.Percent(30),
+        };
+ 
+        var SearchFieldButton = new Button()
+        {
+            X = Pos.Right(SearchFieldText) + 2,
+            Text = "Search",
+        };
+        
+        SearchFieldButton.Clicked += () =>
+        {
+            string SearchValue = (string)SearchFieldText.Text;
+            Dictionary<string, object> newFilter = new Dictionary<string, object>
+            {
+                { "FlightNumber", SearchValue },
+            };
+            Layout.OpenWindow<Admin.Flights.Index>(newFilter);
+        };
+
+        List<Flight> flightView = new List<Flight>();
+        flightView = filters == null ? Flight.All() : Flight.Search(filters);
+        
+        var list = new ListView(flightView)
+        {
+            Y = Pos.Bottom(SearchFieldText) + 2,
             Width = Width,
             Height = Dim.Fill(),
         };
-
+        
         list.OpenSelectedItem += f =>
         {
             Layout.OpenWindow<Show>(f.Value);
         };
         
-        Add(list);
+        Add(SearchFieldText, SearchFieldButton, list);
     }
 }
