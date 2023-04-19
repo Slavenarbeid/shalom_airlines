13b020a655d9d4e9c38ee39d63a783ae98e4f9f5
Rik R <rik@remijn.org>
2023-04-19 15:26:59 +0200

    base for new layout class.

---
+++ backend/Models/Layout.cs
@@ -0,0 +1,6 @@
+﻿namespace backend.Models;
+
+public class Layout
+{
+    public Dictionary<string, Dictionary<string, Dictionary<int, bool>>> SeatLayout;
+}
---
+++ backend/program.cs
@@ -0,0 +1,11 @@
+﻿using backend.Controllers;
+
+public class Program
+{
+    public static void Main()
+    {
+        var testLayout = PlaneController.CreateSeatingLayout(5, 4, 10, 15);
+        
+        Console.WriteLine(testLayout);
+    }
+}
