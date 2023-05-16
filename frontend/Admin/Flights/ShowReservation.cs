5d0adb547b0a06dcb402d44b23c37f9992b0459c
TijmenSosef <73581008+TijmenSosef@users.noreply.github.com>
2023-05-17 01:57:46 +0200

    +

---
+++ frontend/Admin/Flights/ShowReservation.cs
@@ -0,0 +1,14 @@
+﻿using backend.Controllers;
+using backend.Models;
+using Terminal.Gui;
+using Attribute = Terminal.Gui.Attribute;
+
+namespace shalom_airlines.Admin.Flights;
+
+public class ShowReservation : Window
+{
+    public ShowReservation(Seat seat)
+    {
+        
+    }
+}
